419adb8ee8f643fea3cbd51460ccba9207c40015
akhiilkasare <34769726+akhiilkasare@users.noreply.github.com>
2021-01-02 00:55:06 +0530

    Add files via upload

---
+++ arrays/reverser-an-array.py
@@ -0,0 +1,9 @@
+# Given an array (or string), the task is to reverse the array/string.
+
+def reverseList(A):
+
+    return A[::-1]
+
+a = [1,2,3,4]
+
+print("The reversed list is : ", reverseList(a))
